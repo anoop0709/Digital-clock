9185bf54a0778ced509c34a24c738d0cf004fecb
Anoop sukumaran nair <92984490+anoop0709@users.noreply.github.com>
2022-03-24 18:59:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const display = document.getElementById('clock');
+
+
+function updateTime(){
+    const date= new Date();
+
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    
+    display.innerText = `${hour} : ${minutes} : ${seconds}`;
+
+}
+ 
+setInterval(updateTime, 1000);
